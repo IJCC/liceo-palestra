7d0a3ad18a38d1c1ea6a9501d23cb7173898f298
IJCC <ijcc.des@gmail.com>
2023-08-22 00:11:53 -0600

    Update index
    Trabajando en la información final proporcionada...

--- index.php
+++ index.php
@@ -168,44 +168,77 @@
   </section>
   <!-- End service  -->
 
-  <!-- Start about us -->
-  <section id="mu-about-us">
+  <!-- Start from blog -->
+  <section id="mu-from-blog">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="mu-about-us-area">           
-            <div class="row">
-              <div class="col-lg-6 col-md-6">
-                <div class="mu-about-us-left">
-                  <!-- Start Title -->
-                  <div class="mu-title">
-                    <h2>About Us</h2>              
-                  </div>
-                  <!-- End Title -->
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum pariatur fuga eveniet soluta aspernatur rem, nam voluptatibus voluptate voluptates sapiente, inventore. Voluptatem, maiores esse molestiae.</p>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-                    <li>Saepe a minima quod iste libero rerum dicta!</li>
-                    <li>Voluptas obcaecati, iste porro fugit soluta consequuntur. Veritatis?</li>
-                    <li>Ipsam deserunt numquam ad error rem unde, omnis.</li>
-                    <li>Repellat assumenda adipisci pariatur ipsam eos similique, explicabo.</li>
-                  </ul>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quaerat harum facilis excepturi et? Mollitia!</p>
-                </div>
-              </div>
-              <div class="col-lg-6 col-md-6">
-                <div class="mu-about-us-right">                            
-                <a id="mu-abtus-video" href="https://www.youtube.com/embed/HN3pm9qYAUs" target="mutube-video">
-                  <img src="assets/img/about-us.jpg" alt="img">
-                </a>                
-                </div>
-              </div>
+          <div class="mu-from-blog-area">
+            <!-- start title -->
+            <div class="mu-title">
+              <h2>Conoce la Opinión de Nuestros Alumnos</h2>
             </div>
+            <!-- end title -->  
+            <!-- start from blog content   -->
+            <div class="mu-from-blog-content">
+              <div class="row">
+                <div class="col-md-4 col-sm-4">
+                  <article class="mu-blog-single-item">
+                    <figure class="mu-blog-single-img">
+                      <!--<a href="#"><img src="assets/img/blog/blog-1.jpg" alt="img"></a>-->
+                      <a id="mu-abtus-video" href="https://www.youtube.com/watch?v=j0bZvo8LcJI&t=126s" target="mutube-video">
+                        <img src="assets/img/about-us.jpg" alt="img">
+                      </a>  
+                      <figcaption class="mu-blog-caption">
+                        <h5>"Hacemos Nuestra Academia Aún Más Divertida"</h5>
+                      </figcaption>                      
+                    </figure>
+                    <div class="mu-blog-meta"> 
+                    </div>
+                  </article>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                  <article class="mu-blog-single-item">
+                  <figure class="mu-blog-single-img">
+                      <!--<a href="#"><img src="assets/img/blog/blog-1.jpg" alt="img"></a>-->
+                      <a id="mu-abtus-video" href="https://www.youtube.com/watch?v=YKgdVSmMwjo" target="mutube-video">
+                        <img src="assets/img/about-us.jpg" alt="img">
+                      </a>  
+                      <figcaption class="mu-blog-caption">
+                        <h5>"Su Voz Cuenta: Mejorando Juntos Nuestra Experiencia"</h5>
+                      </figcaption>                          
+                    </figure>
+                    <div class="mu-blog-meta">
+                    </div>
+                  </article>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                  <article class="mu-blog-single-item">
+                  <figure class="mu-blog-single-img">
+                      <!--<a href="#"><img src="assets/img/blog/blog-1.jpg" alt="img"></a>-->
+                      <a id="mu-abtus-video" href="https://www.youtube.com/watch?v=YKgdVSmMwjo" target="mutube-video">
+                        <img src="assets/img/about-us.jpg" alt="img">
+                      </a>  
+                      <figcaption class="mu-blog-caption">
+                      <h5>"Pendiente"</h5>
+                      </figcaption>                      
+                    </figure>
+                    <div class="mu-blog-meta">
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>     
+            <!-- end from blog content   -->   
           </div>
         </div>
       </div>
     </div>
   </section>
+  <!-- End from blog -->
+
+  <!-- Start about us -->
+  
   <!-- End about us -->
 
   <!-- Start about us counter -->
@@ -267,59 +300,21 @@
           <div class="mu-features-area">
             <!-- Start Title -->
             <div class="mu-title">
-              <h2>Our Features</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsa ea maxime mollitia, vitae voluptates, quod at, saepe reprehenderit totam aliquam architecto fugiat sunt animi!</p>
+              <h2>Nuestra Visión</h2>
             </div>
             <!-- End Title -->
             <!-- Start features content -->
             <div class="mu-features-content">
               <div class="row">
-                <div class="col-lg-4 col-md-4  col-sm-6">
+                <div class="col-lg-12">
                   <div class="mu-single-feature">
-                    <span class="fa fa-book"></span>
-                    <h4>Professional Courses</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.</p>
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                  <div class="mu-single-feature">
-                    <span class="fa fa-users"></span>
-                    <h4>Expert Teachers</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.</p>
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                  <div class="mu-single-feature">
-                    <span class="fa fa-laptop"></span>
-                    <h4>Online Learning</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.</p>
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                  <div class="mu-single-feature">
-                    <span class="fa fa-microphone"></span>
-                    <h4>Audio Lessons</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.</p>
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                  <div class="mu-single-feature">
-                    <span class="fa fa-film"></span>
-                    <h4>Video Lessons</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.</p>
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                  <div class="mu-single-feature">
-                    <span class="fa fa-certificate"></span>
-                    <h4>Professional Certificate</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.</p>
-                    <a href="#">Read More</a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus non dolorem excepturi libero itaque sint labore similique maxime natus eum.
+                    </p>
                   </div>
                 </div>
               </div>
@@ -635,13 +630,13 @@
       <div class="row">
         <div class="col-md-12">
           <div class="mu-from-blog-area">
-            <!-- start title -->
+            <!-- start title 
             <div class="mu-title">
               <h2>From Blog</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum vitae quae vero ut natus. Dolore!</p>
-            </div>
+            </div>-->
             <!-- end title -->  
-            <!-- start from blog content   -->
+            <!-- start from blog content   
             <div class="mu-from-blog-content">
               <div class="row">
                 <div class="col-md-4 col-sm-4">
@@ -703,7 +698,7 @@
                 </div>
               </div>
             </div>     
-            <!-- end from blog content   -->   
+             end from blog content   -->   
           </div>
         </div>
       </div>
